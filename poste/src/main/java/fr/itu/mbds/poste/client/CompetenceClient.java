fc5fc9b6498ecb262f4e3944d66ef85d58bbe72e
RickyBic <99736616+RickyBic@users.noreply.github.com>
2025-05-23 11:47:43 +0300

    Sécurisation des Communications avec Resilience4J (Poste)

--- poste/src/main/java/fr/itu/mbds/poste/client/CompetenceClient.java
+++ poste/src/main/java/fr/itu/mbds/poste/client/CompetenceClient.java
@@ -1,6 +1,7 @@
 package fr.itu.mbds.poste.client;
 
 import fr.itu.mbds.poste.model.Competence;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +11,21 @@ import java.util.List;
 @FeignClient(name = "COMPETENCE")
 public interface CompetenceClient {
     @GetMapping("/skills")
+    @CircuitBreaker(name = "competence", fallbackMethod = "fallbackGetAllCompetences")
     List<Competence> getAllCompetences();
 
     @GetMapping("/skill/{id}")
+    @CircuitBreaker(name = "competence", fallbackMethod = "fallbackGetCompetenceById")
     Competence getCompetenceById(@PathVariable Long id);
+
+    default List<Competence> fallbackGetAllCompetences(Exception e) {
+        return List.of();
+    }
+
+    default Competence fallbackGetCompetenceById(Long id, Exception e) {
+        Competence competence = new Competence();
+        competence.setId(id);
+        competence.setNom("Not available");
+        return competence;
+    }
 }
