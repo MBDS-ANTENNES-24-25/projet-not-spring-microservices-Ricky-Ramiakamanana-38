b03b81ab2e8014bc8895b17f374d34fa3dcb6e85
RickyBic <99736616+RickyBic@users.noreply.github.com>
2025-05-23 11:36:18 +0300

    Mise en Place du Gateway API

--- poste/src/main/java/fr/itu/mbds/poste/client/CompetenceClient.java
+++ poste/src/main/java/fr/itu/mbds/poste/client/CompetenceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "competence", url = "http://localhost:8080")
+@FeignClient(name = "COMPETENCE")
 public interface CompetenceClient {
     @GetMapping("/skills")
     List<Competence> getAllCompetences();
